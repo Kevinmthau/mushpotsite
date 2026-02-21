7bfd299417cc1a05aa9d77659584c7f9551c1410
Kevin Thau <kevinthau@Kevins-MacBook-Pro.local>
2026-02-20 21:42:12 -0500

    Optimize home page image assets

--- src/data/homePageItems.ts
+++ src/data/homePageItems.ts
@@ -1,17 +1,17 @@
 import type { HomePageItem } from './types';
 
 export const homePageItems: HomePageItem[] = [
-  { type: 'link', href: 'http://board.fun', src: '/images/board.png', alt: 'Board', size: 'large', noReflect: true },
-  { type: 'link', href: 'https://testflight.apple.com/join/1MC5wtQe', src: '/images/wallet-icon.png', alt: 'Wallet', size: 'small', noReflect: true },
-  { type: 'div', src: '/images/esc-app-icon.png', alt: 'Esc', size: 'small', noReflect: true },
+  { type: 'link', href: 'http://board.fun', src: '/images/board-home.jpg', alt: 'Board', size: 'large', noReflect: true },
+  { type: 'link', href: 'https://testflight.apple.com/join/1MC5wtQe', src: '/images/wallet-icon-home.jpg', alt: 'Wallet', size: 'small', noReflect: true },
+  { type: 'div', src: '/images/esc-app-icon-home.jpg', alt: 'Esc', size: 'small', noReflect: true },
   { type: 'link', href: 'https://kevins-outfit-finder.netlify.app/kevin/', src: '/images/Outfit-finder.jpg', alt: 'Outfit Finder', size: 'small', noReflect: true },
   { type: 'link', href: 'https://testflight.apple.com/join/VppMNjcA', src: '/images/Whodiss.jpg', alt: 'Whodiss', size: 'small', noReflect: true },
-  { type: 'link', href: '/1040', src: '/images/1040.jpg', alt: '1040', size: 'large', noReflect: true },
+  { type: 'link', href: '/1040', src: '/images/1040-home.jpg', alt: '1040', size: 'large', noReflect: true },
   { type: 'div', src: '/images/twitter-music.jpg', alt: 'Twitter Music', noReflect: true },
-  { type: 'link', href: '/ios5-features.html', src: '/images/Twitter-IOS5-integration.png', alt: 'Twitter iOS 5', size: 'xsmall', noReflect: true },
-  { type: 'video', videoSrc: '/images/Twitter for iPad 1.0 Demo.mp4', src: '/images/Twitter-for-ipad.jpg', alt: 'Twitter for iPad', noReflect: true },
-  { type: 'div', src: '/images/twitter_for_iphone.png', alt: 'Twitter for iPhone', noReflect: true },
+  { type: 'link', href: '/ios5-features.html', src: '/images/Twitter-IOS5-integration-home.jpg', alt: 'Twitter iOS 5', size: 'xsmall', noReflect: true },
+  { type: 'video', videoSrc: '/images/Twitter for iPad 1.0 Demo.mp4', src: '/images/Twitter-for-ipad-home.jpg', alt: 'Twitter for iPad', noReflect: true },
+  { type: 'div', src: '/images/twitter_for_iphone-home.jpg', alt: 'Twitter for iPhone', noReflect: true },
   { type: 'div', src: '/images/wap_browser.jpg', alt: 'Openwave', size: 'large', noReflect: true },
   { type: 'div', src: '/images/smtp_pop3_server.gif', alt: 'Post.Office', noReflect: true },
-  { type: 'link', href: 'https://sgi-demos.github.io', src: '/images/SGI.jpg', alt: 'SGI', size: 'large', noReflect: true },
+  { type: 'link', href: 'https://sgi-demos.github.io', src: '/images/SGI-home.jpg', alt: 'SGI', size: 'large', noReflect: true },
 ];
