96c40ca5e6c2188b9f64dd8b2363e8236c266c48
Kevin Thau <kevinthau@Kevins-MacBook-Pro.local>
2026-03-05 11:16:55 -0500

    Add Cingular project card and update asset

--- src/data/homePageItems.ts
+++ src/data/homePageItems.ts
@@ -10,6 +10,7 @@ export const homePageItems: HomePageItem[] = [
   { type: 'link', href: '/ios5-features.html', src: '/images/Twitter-IOS5-integration-home.jpg', alt: 'Twitter iOS 5', size: 'xsmall', noReflect: true },
   { type: 'video', videoSrc: '/images/Twitter for iPad 1.0 Demo.mp4', src: '/images/Twitter-for-ipad-home.jpg', alt: 'Twitter for iPad', noReflect: true },
   { type: 'div', src: '/images/twitter_for_iphone-home.jpg', alt: 'Twitter for iPhone', noReflect: true },
+  { type: 'div', src: '/images/Cingular.PNG', alt: 'Cingular', size: 'medium', noReflect: true },
   { type: 'div', src: '/images/wap_browser.jpg', alt: 'Openwave', size: 'large', noReflect: true },
   { type: 'div', src: '/images/smtp_pop3_server.gif', alt: 'Post.Office', noReflect: true },
   { type: 'link', href: 'https://sgi-demos.github.io', src: '/images/SGI-home.jpg', alt: 'SGI', size: 'large', noReflect: true },
